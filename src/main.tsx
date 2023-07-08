4666d3e71d477cf3e4e990bfc1a63597b6a3c20c
Rayhanendra <mrayhann@gmail.com>
2023-07-08 10:03:56 +0700

    feat: add router and homepage

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
-import theme from './theme';
+import theme from './styles/theme';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
